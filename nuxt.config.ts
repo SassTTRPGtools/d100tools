139b1641f214620d51a40b9af19c1055718c1edd
rosataker <pechin1219@gmail.com>
2024-07-06 07:30:49 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  devtools: { enabled: true }
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
+<template>
+  <div>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">About page</NuxtLink>
+    <div class="hello">
+      Hello Nuxt {{ version }}!
+    </div>
+  </div>
+</template>
